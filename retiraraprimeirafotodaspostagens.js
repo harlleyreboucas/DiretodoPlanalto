b7fbc94697ccacbb85b4eebd65e42229bb35cd5a
Harlley Rebouças <contato@harlley.com.br>
2021-08-04 22:48:27 -0400

    Update retiraraprimeirafotodaspostagens.js

--- retiraraprimeirafotodaspostagens.js
+++ retiraraprimeirafotodaspostagens.js
@@ -1,2 +1,3 @@
-  var firstImg = document.querySelector('.separator img');
+  var firstImg = document.querySelector('.post-body .separator img:nth-child(2)');
         firstImg.style.display='none';
+
